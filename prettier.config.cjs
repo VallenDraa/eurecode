70eac87c7d1657eadf5996a16e123520a7977e4a
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-08-21 13:17:23 +0700

    build: update t3 dependencies and migrate to pnpm

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  arrowParens: "avoid",
+  trailingComma: "all",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  endOfLine: "auto",
 };
 
 module.exports = config;
